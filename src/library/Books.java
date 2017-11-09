688a3ac36fcec89661c1e22781a13e247ae27ba4
Ashoranga <Ashoranga@192.168.1.2>
2017-11-09 20:08:32 +0530

    Library sample in Object oriented programme

---
+++ src/library/Books.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+/**
+ *
+ * @author Ashoranga
+ */
+public class Books {
+    String bname;
+    int bID;
+    String auther;
+    int pageNo;
+    
+    
+            
+    
+}
---
+++ src/library/Member.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+
+/**
+ *
+ * @author Ashoranga
+ */
+public class Member {
+    String name;
+    int mID;
+    String joinDate;
+    Books book;
+    
+    
+}
